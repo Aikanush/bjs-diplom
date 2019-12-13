632423cac11c91fa6a1be7809cd4c3b00471d128
Aikanush <haka.m@mail.ru>
2019-12-13 21:34:44 +0400

    changed main.js

---
+++ static/main.js
@@ -0,0 +1,108 @@
+class Profile {
+  constructor(user) {
+  this.user = user;
+  }
+
+  performLogin(callback) {
+    console.log(`User ${this.user.username} is authorizing`);
+        return ApiConnector.performLogin(this.user, (err, data) => {
+            console.log(`User ${this.user.username} authorized`);
+            callback(err, data);
+        });
+  }
+
+  createUser(callback) {
+         console.log(`Adding new user ${this.user.username}`);
+         return ApiConnector.createUser(this.user, (err, data) => {
+            console.log(`Added new user ${this.user.username}`);
+            callback(err, data);
+        });
+     }
+
+  addMoney({ currency, amount }, callback) {
+        console.log(`Adding ${amount} of ${currency} to ${this.username}`);
+        return ApiConnector.addMoney({ currency, amount }, (err, data) => {
+            console.log(`Added ${amount} of ${currency} to ${this.username}`);
+            callback(err, data);
+        });
+    }
+
+    convertMoney({ fromCurrency, targetCurrency, targetAmount }, callback) {
+      console.log(`Converting ${targetAmount} of ${fromCurrency} to ${targetAmount}`);
+      return ApiConnector.convertMoney({ fromCurrency, targetCurrency, targetAmount }, (err, data) => {
+        console.log(`Converted ${targetAmount} of ${fromCurrency} to ${targetAmount}`);
+        callback(err, data);
+      });
+    }
+
+    transferMoney({ to, amount }, callback) {
+      console.log(`Transfering ${amount} to ${to}`);
+      return ApiConnector.trasferMoney( {to, amount}, (err, data) => {
+        console.log(`Transfered ${amount} to ${to}`);
+        callback(err, data);
+      });
+    }
+}
+
+ const getStocks = (callback) => {
+  return ApiConnector.getStocks((err, data) => {
+    console.log(`Запрос курсов валют с сервера`);
+    callback(err, data);
+  })
+}
+
+function main() {
+    const Alex = new Profile({
+                    username: 'alex',
+                    name: { firstName: 'Alex', lastName: 'Alexeev' },
+                    password: 'alex5555',
+                });
+    const Semen = new Profile({
+                    username: 'semen',
+                    name: { firstName: 'Semen', lastName: 'Bondarev' },
+                    password: 'semen5555',
+                });
+    // сначала создаем и авторизуем пользователя
+    Alex.createUser((err, data) => {
+          if (err) {
+            console.error(`Error adding new user`);
+          } else {
+            console.log(`Added new user ${username}`);
+
+            Alex.performLogin((err, data) => {
+                 if (err) {
+                 console.error(`Error authorizing new user`);
+                 } else {
+                 console.log(`Authorized new user ${username}`);
+
+                  Alex.addMoney({ currency: 'RUB', amount: 100 }, (err, data) => {
+                         if (err) {
+                           console.error(`Error during adding money to ${username}`);
+                         } else {
+                           console.log(`Added ${amount} ${currency} to ${username}`);
+
+                           getStocks((err, data) => {
+                              if (err) {
+                                console.error(`Error getting stocks`);
+                              } else {
+                                console.log(`Got stocks`);
+                                console.log(data);
+
+                                // Alex.convertMoney({ fromCurrency: 'RUB', targetCurrency: 'Netkoins', targetAmount: 100 }, (err, data) => {
+                       
+
+                               //     });
+                              }
+                             });
+
+                         }
+                  });
+                }
+          });
+         }
+       });
+}
+
+           
+
+main();
